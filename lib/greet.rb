16e98c82b0323b812a9a5b52b03d591a37d0cfca
Scully87 <scully87.ts@googlemail.com>
2014-10-31 18:43:58 +0000

    styley

--- lib/greet.rb
+++ lib/greet.rb
@@ -6,7 +6,7 @@ class Greet
 	include DataMapper::Resource
 
 	property :id,         Serial
-	property :body,       String, length: 1..150
+	property :body,       String
 	property :user_name,  String
 	property :name, 	  String
 	property :created_at, DateTime
