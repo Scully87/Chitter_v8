15a9bf9a76bd0895639aad173937fc5d1fdf30ad
Scully87 <scully87.ts@googlemail.com>
2014-11-03 09:44:03 +0000

    updated time stamp

--- lib/greet.rb
+++ lib/greet.rb
@@ -9,7 +9,7 @@ class Greet
 	property :body,       String
 	property :user_name,  String
 	property :name, 	  	String
-	property :created_at, DateTime
+	property :created_at, DateTime, :default => lambda{ |p,s| DateTime.now}
 
 	has 1, :user, :through => Resource
 
